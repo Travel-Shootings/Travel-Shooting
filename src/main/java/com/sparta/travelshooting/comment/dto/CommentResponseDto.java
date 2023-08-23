a43d1e4a9654bbca30258a2bacb66bb6e288ef8e
Inseo Kim <itsinseo@gmail.com>
2023-08-23 22:13:50 +0900

    CommentResponseDto Getter 추가

--- src/main/java/com/sparta/travelshooting/comment/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/travelshooting/comment/dto/CommentResponseDto.java
@@ -1,9 +1,11 @@
 package com.sparta.travelshooting.comment.dto;
 
 import com.sparta.travelshooting.comment.entity.Comment;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class CommentResponseDto {
     private Long id;
     private String nickName;
@@ -11,13 +13,11 @@ public class CommentResponseDto {
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 
-
     public CommentResponseDto(Comment comment) {
         this.id = comment.getCommentId();
         this.nickName = comment.getNickName();
         this.content = comment.getContent();
         this.createdAt = comment.getCreatedAt();
         this.modifiedAt = comment.getModifiedAt();
-
     }
 }
