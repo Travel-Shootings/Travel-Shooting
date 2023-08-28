250f8bd1192c523983e90ee11bc75a37a3078ad3
BenKim95 <forseminaar@gmail.com>
2023-08-28 20:14:20 +0900

    Post 프론트 엔드 구현중

--- src/main/java/com/sparta/travelshooting/journeylist/controller/JourneyListViewController.java
+++ src/main/java/com/sparta/travelshooting/journeylist/controller/JourneyListViewController.java
@@ -1,16 +1,14 @@
-package com.sparta.travelshooting.post.controller;
+package com.sparta.travelshooting.journeylist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class PostViewController {
+public class JourneyListViewController {
 
-    @GetMapping("/test")
-    public String test () {
-        return "create-post";
+    @GetMapping("/journey")
+    public String journey() {
+        return "journeyList";
     }
-
-
 }
--- src/main/java/com/sparta/travelshooting/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta/travelshooting/post/dto/PostRequestDto.java
@@ -1,9 +1,6 @@
 package com.sparta.travelshooting.post.dto;
 
-import com.sparta.travelshooting.journeylist.entity.JourneyList;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 public class PostRequestDto {
