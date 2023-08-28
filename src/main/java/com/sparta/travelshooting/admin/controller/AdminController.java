fa3e7ebf1b1d0ef61f0a72d74568e56dcc774430
Inseo Kim <itsinseo@gmail.com>
2023-08-28 21:32:50 +0900

    Controller 애노테이션으로 통일

--- src/main/java/com/sparta/travelshooting/admin/controller/AdminController.java
+++ src/main/java/com/sparta/travelshooting/admin/controller/AdminController.java
@@ -14,7 +14,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/sparta/travelshooting/journeylist/controller/JourneyListController.java
+++ src/main/java/com/sparta/travelshooting/journeylist/controller/JourneyListController.java
@@ -4,19 +4,13 @@ import com.sparta.travelshooting.common.ApiResponseDto;
 import com.sparta.travelshooting.journeylist.dto.JourneyListRequestDto;
 import com.sparta.travelshooting.journeylist.dto.JourneyListResponseDto;
 import com.sparta.travelshooting.journeylist.service.JourneyListServiceImpl;
-import com.sparta.travelshooting.journeylist.service.JourneyListService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @Controller
