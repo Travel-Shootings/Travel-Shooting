b1d6cbc83dd1ca8479eab23fa59bbf198e44c10a
sehun <ban6276@gmail.com>
2023-08-31 21:36:48 +0900

    수정 기능 1차 완성

--- src/main/resources/static/js/updateReview.js
+++ src/main/resources/static/js/updateReview.js
@@ -1,3 +1,22 @@
+const imageInput = document.getElementById('images');
+const selectedImageNamesContainer = document.getElementById('selectedImageNamesContainer');
+const selectedImageNames = []; // 이미지 파일명을 저장하는 배열
+
+imageInput.addEventListener('change', () => {
+    // 기존 목록 초기화
+    selectedImageNamesContainer.innerHTML = '';
+
+    // 선택한 이미지 파일들의 목록을 업데이트
+    for (let i = 0; i < imageInput.files.length; i++) {
+        const imageFile = imageInput.files[i];
+
+        // 이미지 파일명을 목록에 추가해서 표시
+        const imageItem = document.createElement('li');
+        imageItem.textContent = imageFile.name;
+        selectedImageNamesContainer.appendChild(imageItem);
+    }
+});
+
 const submitButton = document.getElementById('submit-button'); // 수정 페이지의 "저장" 버튼
 const cancelButton = document.getElementById('cancel-button'); // 수정 페이지의 "취소" 버튼
 
@@ -27,14 +46,15 @@ async function updateReviewPost() {
     // 필요한 데이터 수집 및 validation
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
-    const imageFiles = document.getElementById('images').files;
+    const images = document.getElementById('images').files;
 
     const formData = new FormData();
     formData.append('title', title);
     formData.append('content', content);
 
-    for (const file of imageFiles) {
-        formData.append('imageFiles', file);
+    // 여러 개의 이미지 파일을 FormData에 추가
+    for (let i = 0; i < images.length; i++) {
+        formData.append('images', images[i]);
     }
 
     try {
@@ -43,17 +63,18 @@ async function updateReviewPost() {
             body: formData,
         });
 
-        if (response.ok) {
+
             // 수정 성공 시 처리
-            window.location.href = '/view/reviewPost'; // 수정된 후기 게시글 페이지로 이동
-        } else {
-            console.error('Error updating review post:', response.statusText);
-        }
+            const confirmation = confirm('게시글이 수정되었습니다. 확인을 누르면 목록으로 이동합니다.');
+
+            // 확인을 누를 경우 /view/reviewPost 페이지로 리디렉션
+            window.location.href = '/view/reviewPost';
+
+
     } catch (error) {
         console.error('Error updating review post:', error);
     }
 }
-
 // 수정 취소 함수
 function cancelUpdate() {
     // 수정 취소 시 처리
