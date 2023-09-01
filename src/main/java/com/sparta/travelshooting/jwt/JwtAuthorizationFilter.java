3c5505d0db5944e7b35c919b31011b8c2141ab18
Inseo Kim <itsinseo@gmail.com>
2023-09-01 18:42:54 +0900

    정적 리소스 로딩시에는 cookie 검사하지 않도록 수정

--- src/main/java/com/sparta/travelshooting/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/travelshooting/jwt/JwtAuthorizationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -46,7 +47,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 return;
             }
         }
-
+        log.warn("JwtAuthorizationFilter.doFilterInternal");
         filterChain.doFilter(req, res);
     }
 
